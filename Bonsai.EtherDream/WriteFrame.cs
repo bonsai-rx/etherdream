8d3edf8f2878772e46f2ec8642288f9642178e57
glopes <glopesdev@users.noreply.github.com>
2017-12-26 19:01:39 +0000

    Added first draft of WriteFrame sink.

---
+++ Bonsai.EtherDream/WriteFrame.cs
@@ -0,0 +1,42 @@
+﻿using EtherDream.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.EtherDream
+{
+    public class WriteFrame : Sink<EtherDreamPoint[]>
+    {
+        [TypeConverter(typeof(DeviceNameConverter))]
+        public string DeviceName { get; set; }
+
+        public int PointsPerSecond { get; set; }
+
+        public int Repetitions { get; set; }
+
+        public override IObservable<EtherDreamPoint[]> Process(IObservable<EtherDreamPoint[]> source)
+        {
+            return Observable.Using(
+                () => EtherDreamDevice.GetDevices().First(device => device.Name == DeviceName),
+                device => source.Do(input => device.WriteFrame(input, PointsPerSecond, Repetitions)));
+        }
+
+        class DeviceNameConverter : StringConverter
+        {
+            public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+            {
+                return true;
+            }
+
+            public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+            {
+                var deviceNames = EtherDreamDevice.GetDevices().Select(device => device.Name).ToArray();
+                return new StandardValuesCollection(deviceNames);
+            }
+        }
+    }
+}
